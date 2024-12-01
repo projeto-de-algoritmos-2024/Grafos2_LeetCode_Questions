539571ed173a3b0b74e7114c4ae358cb5d0baef6
PedroHenrique2077 <pedro.rodriguesdecarvalho080@gmail.com>
2024-12-01 15:03:35 -0300

    Correção da sobrescrição do commit do Lucas da questão 1368 ~ 42ms

--- 1368. Minimum Cost to Make at Least One Valid Path in a Grid/1368.ts
+++ 1368. Minimum Cost to Make at Least One Valid Path in a Grid/1368.ts
@@ -10,16 +10,16 @@ function minCost(grid: number[][]): number {
     ];
 
     const dist = Array.from({ length: m }, () => Array(n).fill(Infinity));
-    const queue: [number, number, number][] = [];
+    const deque: [number, number, number][] = []; 
 
     const inBounds = (x: number, y: number) => x >= 0 && x < m && y >= 0 && y < n;
 
+    // Initialize
     dist[0][0] = 0;
-    queue.push([0, 0, 0]);
+    deque.push([0, 0, 0]); 
 
-    while (queue.length > 0) {
-        queue.sort((a, b) => a[0] - b[0]);
-        const [currentCost, x, y] = queue.shift()!;
+    while (deque.length > 0) {
+        const [currentCost, x, y] = deque.shift()!;
 
         if (currentCost > dist[x][y]) continue;
 
@@ -29,12 +29,16 @@ function minCost(grid: number[][]): number {
             const ny = y + dy;
 
             if (!inBounds(nx, ny)) continue;
-
             const newCost = currentCost + (grid[x][y] === d + 1 ? 0 : 1);
 
             if (newCost < dist[nx][ny]) {
                 dist[nx][ny] = newCost;
-                queue.push([newCost, nx, ny]);
+
+                if (grid[x][y] === d + 1) {
+                    deque.unshift([newCost, nx, ny]);
+                } else {
+                    deque.push([newCost, nx, ny]);
+                }
             }
         }
     }
